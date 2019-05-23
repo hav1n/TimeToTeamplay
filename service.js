938505fcd106da1a47520e57133f8928c48e47d6
hav1n <ghwjd0816@gmail.com>
2019-05-23 12:57:42 +0900

    404 not found commit

--- service.js
+++ service.js
@@ -4,20 +4,25 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+
     console.log(queryData);
-    if(_url == '/'){
-      _url = '/index.html';
+    if(pathname === '/')
+    {
+      if(_url == '/'){
+        _url = '/index.html';
+      }
+      else{
+        _url = `/${queryData.id}/${queryData.page}`;
+      }
+      response.writeHead(200);
+      console.log(_url);
+      response.end(fs.readFileSync(__dirname+_url));
     }
-    else if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-      response.end();
-      return;
+    else {
+      response.writeHead(404);
+      console.log(_url+" Not Found");
+      response.end('404 Not found');
     }
-    else{
-      _url = `/${queryData.id}/${queryData.page}`;
-    }
-    response.writeHead(200);
-    console.log(_url);
-    response.end(fs.readFileSync(__dirname+_url));
 });
 app.listen(8000);
